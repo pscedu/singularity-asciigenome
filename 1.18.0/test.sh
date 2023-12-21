ad493eb1806f41e09e136396b2b5afc70f473332
Ivan Cao-Berg <icaoberg@andrew.cmu.edu>
2023-12-21 07:16:32 -0500

    Added version 1.18.0

---
+++ 1.18.0/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -x
+
+./ASCIIGenome -ni http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/functional_annotation/filtered/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.annotation.vcf.gz
